ac62d3a8a4cc75401166fcd649c786cd899e1419
Renanincio <renanincioprgmdr@gmail.com>
2022-12-02 20:32:45 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { About } from "../components/About";
 import { Footer } from "../components/Footer";
 import { HomePage } from "../components/Home";
@@ -8,6 +9,10 @@ import { MySkills } from "../components/Skills";
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Portfólio Website</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <HomePage />
       <MySkills />
       <Projects />
